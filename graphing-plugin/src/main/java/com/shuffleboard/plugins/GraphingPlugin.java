7160c9d6c0f400caedbfad2396156025ffbce7f4
Nikhil Iyer <iyer.h.nikhil@gmail.com>
2022-02-23 18:22:36 -0500

    start graphing plugin

---
+++ graphing-plugin/src/main/java/com/shuffleboard/plugins/GraphingPlugin.java
@@ -0,0 +1,14 @@
+package com.shuffleboard.plugins;
+
+import edu.wpi.first.shuffleboard.api.plugin.Description;
+import edu.wpi.first.shuffleboard.api.plugin.Plugin;
+
+/**
+ * A base plugin for demonstration purposes.
+ * 
+ * See https://docs.wpilib.org/en/stable/docs/software/dashboards/shuffleboard/custom-widgets/creating-plugins.html for more info.
+ */
+@Description(group = "com.shuffleboard.plugins", name = "GraphingPlugin", version = "1.0.0", summary = "A plugin for plotting lists of points.")
+public class GraphingPlugin extends Plugin {
+    // Use this as a template for how a plugin should be structured.
+}
